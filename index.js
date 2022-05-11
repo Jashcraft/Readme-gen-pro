b4899c0fbf052a8ffe9aebf79b83fd3cdf6b23eb
Jashcraft <ashcraft.jonathon@gmail.com>
2022-05-10 19:28:52 -0600

    I've written some questions

---
+++ index.js
@@ -0,0 +1,69 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of this Project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of this Project?",
+  },
+  {
+    type: "input",
+    name: "installationInstructions",
+    message: "What steps are neeeded to install this Project?",
+  },
+  {
+    type: "input",
+    name: "usageInformation",
+    message: "How do you use this Project?",
+  },
+  {
+    type: "input",
+    name: "contributionGuidelines",
+    message: "What (in theory) would a person (or persons) need to know to contribute to this PARTICULAR Project?",
+  },
+  {
+    type: "input",
+    name: "testInstructions",
+    message: "How do you test this Project?",
+  },
+  {
+    type: "list",
+    name: "badgeLicense",
+    message: "Describe the license for this Project.",
+    choices: ["The Hippocratic License 2.1", "GNU GPL v3", "WTFPL", "Apache 2.0 License", "A Fire Truck"]
+  },
+  {
+    type: "input",
+    name: "gitHubName",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then(answers => {
+    const readMeData = generateMarkdown(answers);
+    console.log(readMeData);
+  })
+}
+
+// Function call to initialize app
+init();
